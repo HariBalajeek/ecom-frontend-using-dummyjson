d824b1e7b86676228db03a3736ff09ade3adc9d2
Hari Balajee <91687445+HariBalajeek@users.noreply.github.com>
2025-04-03 10:58:36 +0530

    Add files via upload
    first

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import { Cart } from './pages/Cart'
+import Login from './pages/Login'
+import PrivateRoute from './routes/PrivateRoute'
+import Footer from './components/Footer'
+import ReviewsAndFooter from './components/Footer'
+
+const App = () => {
+  return (
+    <>
+    <div className='bg-gray-100 min-h-screen flex flex-col'>
+      <NavBar/>
+      <div>
+          <Routes>
+            <Route index element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/cart' element={<PrivateRoute><Cart/></PrivateRoute>}/>
+            <Route path='/login' element={<Login/>}/>
+          </Routes>
+          <ReviewsAndFooter/>
+      </div>
+   
+    </div>
+    </>
+    
+  )
+}
+
+export default App
